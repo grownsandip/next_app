fb9ca94a714d6a9e7e356fc16107cff442f8dc2d
Sandip <roysandip33290@gmail.com>
2024-08-12 20:05:06 +0530

    initail commits

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks=[
+    {
+       labels:'Home',
+       route:'/',
+       imgUrl:'/icons/home.svg',
+    },
+    {
+        labels:'Upcoming',
+        route:'/upcoming',
+        imgUrl:'/icons/upcoming.svg',
+    },
+    {
+        labels:'Previous',
+        route:'/previous',
+        imgUrl:'/icons/previous.svg',
+    },
+    {
+        labels:'Recordings',
+        route:'/recording',
+        imgUrl:'/icons/Videos.svg',
+    },
+    {
+        labels:'Personal room',
+        route:'/personal-room',
+        imgUrl:'/icons/personal-add.svg',
+    },
+]
