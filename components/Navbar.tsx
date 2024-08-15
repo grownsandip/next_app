71ba0f922e485dfa743a26696a6b313f30086071
Sandip <roysandip33290@gmail.com>
2024-08-15 09:40:04 +0530

    added the clerk auth and modified its looks

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,24 +2,27 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
     <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10 '>
       <Link href='/' className='flex items-center gap-1'>
-      <Image
-      src="/icons/logo.svg"
-      alt=''
-      height={32}
-      width={32}
-      className='max-sm:size-10'
-      />
-      <p className='text-[26px] font-extrabold max-sm:hidden text-white'>Zoom</p>
+        <Image
+          src="/icons/logo.svg"
+          alt=''
+          height={32}
+          width={32}
+          className='max-sm:size-10'
+        />
+        <p className='text-[26px] font-extrabold max-sm:hidden text-white'>Zoom</p>
       </Link>
       <div className='flex-between gap-5 bg-dark-1'>
-        {/* clerk user managemant */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         {/* mobile nav */}
-        <MobileNav/>
+        <MobileNav />
       </div>
     </nav>
   )
