df9d412043830d6f02f602481004acc5f2b58133
Sandip <roysandip33290@gmail.com>
2024-08-18 09:27:43 +0530

    call stats and layout button and participants side bar added

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,6 +1,8 @@
 import { cn } from '@/lib/utils';
-import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
+import { CallControls, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
 import React, { useState } from 'react'
+import { DropdownMenu,DropdownMenuContent,DropdownMenuItem,DropdownMenuLabel,DropdownMenuSeparator,DropdownMenuTrigger } from './ui/dropdown-menu';
+import { LayoutList, Users } from 'lucide-react';
 
  type callLayoutType='grid'|'speaker-left'|'speaker-right';
 const MeetingRoom = () => {
@@ -29,6 +31,30 @@ const MeetingRoom = () => {
       </div>
       <div className='fixed w-full bottom-0 flex items-center justify-center gap-5'>
         <CallControls/>
+        <DropdownMenu>
+          <div className='flex items-center '>
+          <DropdownMenuTrigger className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
+            <LayoutList size={20} className='text-white'/>
+          </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent className='border-dark-1 bg-dark-1 text-white '>
+           {['Grid','Speaker-Left','Speaker-Right'].map((item,index)=>(<div key={index}>
+              <DropdownMenuItem className='cursor-pointer hover:bg-[#4c535b] font-bold' onClick={()=>{
+            setLayout(item.toLowerCase() as callLayoutType)
+           }}>
+                {item}
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className='border-dark-1'/>
+              </div>
+           ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <CallStatsButton/>
+        <button onClick={()=>{setShowParticipants((prev)=>!prev)}}>
+          <div className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
+            <Users size={20} className='text-white'/>
+          </div>
+        </button>
       </div>
     </section>
   )
